b47cdcc89296f220a56338337d6b64af34f39fc6
Evgeniy Baranov <master@kompolom.ru>
2016-08-23 10:46:38 +0300

    add glue-filed radio-group type

---
+++ common.blocks/glue-field/_type/glue-field_type_radio-group.js
@@ -0,0 +1,37 @@
+modules.define('glue-field', ['i-bem__dom'], function(provide, BEMDOM) {
+
+provide(BEMDOM.decl({ block : 'glue-field', modName : 'type', modVal : 'radio-group' }, {
+
+    onSetMod : {
+        js : {
+            inited : function() {
+                this.__base();
+                this.input = this.findBlockOn('radio-group');
+            }
+        }
+    },
+
+    init: function() {
+        this.__base.apply(this, arguments);
+
+        this.input
+            .on('change', function() {
+                this.model.set(this.name, this.input.getVal());
+            }, this)
+            .on('blur', function() {
+                this.input.setVal(this.model.get(this.name, 'format'));
+            }, this);
+    },
+
+    set: function(value) {
+        this.__base();
+        this.input.setVal(value);
+    },
+
+    onFieldChange: function(e, data) {
+        this.input.hasMod('focused') || this.input.setVal(data.value);
+    }
+}));
+
+});
+
