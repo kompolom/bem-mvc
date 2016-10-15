f32376ac21313a593a3d85bebb354645fe9231f9
Evgeniy Baranov <master@kompolom.ru>
2016-10-15 14:56:26 +0300

    fix textarea

--- common.blocks/glue-field/_type/glue-field_type_textarea.js
+++ common.blocks/glue-field/_type/glue-field_type_textarea.js
@@ -1,16 +1,38 @@
 modules.define('glue-field', ['i-bem__dom'], function(provide, BEMDOM) {
 
-provide(BEMDOM.decl({ block: 'glue-field', modName : 'type', modVal : 'textarea' }, {
+    provide(BEMDOM.decl({ block: 'glue-field', modName : 'type', modVal : 'textarea'}, {
 
-    onSetMod: {
-        js: {
-            inited: function() {
-                this.__base();
-                this.input = this.findBlockOn('textarea');
+        onSetMod: {
+            js: {
+                inited: function() {
+                    this.__base();
+                    this.input = this.findBlockOn('textarea');
+                }
             }
+        },
+
+        init: function() {
+            this.__base.apply(this, arguments);
+
+            this.input
+                .on('change', function() {
+                    this.model.set(this.name, this.input.getVal());
+                }, this)
+                .on('blur', function() {
+                    this.input.setVal(this.model.get(this.name, 'format'));
+                }, this);
+        },
+
+        set: function(value) {
+            this.__base();
+            this.input.setVal(value);
+        },
+
+        onFieldChange: function(e, data) {
+            this.input.hasMod('focused') || this.input.setVal(data.value);
         }
-    },
-}));
+
+    }));
 
 });
 
